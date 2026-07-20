efe3815ef2740dae93626481f10dbe7ab7ebaf4c
Alexandria-DB <alexa42494@gmail.com>
2026-07-19 22:22:16 -0500

    implemented list active sessions

---
+++ list_active_sessions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from requests import get
+from load_config import load_config
+from utils import *
+
+def list_active_sessions(cfg):
+    url = standard_url(cfg, "session")
+    return get(url).json
+
+if __name__ == "__main__":
+    try:
+        cfg = load_config()
+        for session in list_active_sessions(cfg):
+            print(session)
+    except Exception as ex:
+        print(ex)
+        exit(-1)
